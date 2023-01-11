35489ff1aeba42473449b797446211e8eca17a48
alexander kovtun <kovtuna@getsorbet.com>
2023-01-11 20:21:50 +0200

    first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { LogoSvg } from "./components/logoSvg";
+import image1 from "./images/image2.png";
 
 function App() {
+  const addToChrome = () => {
+    console.log("bla add to chrome");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          2Edit <code>src/App.tsx</code> and save to reload.
+    <div className="container">
+      <img src={image1} alt={"about"} />
+      <div className="content">
+        <LogoSvg />
+        <p className="logoDescription">PTO Shopping Companion</p>
+        <p className="title">
+          Level up your shopping
+          <br />
+          experience with
+          <br />
+          unused paid time off{" "}
         </p>
-      </header>
+        <p className="description">
+          Get instant cash by signing up and applying.
+        </p>
+        <button onClick={addToChrome} className="button">
+          Add to Chrome - It’s Free
+        </button>
+      </div>
     </div>
   );
 }
