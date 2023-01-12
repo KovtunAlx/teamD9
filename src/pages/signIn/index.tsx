5eb37b80917d23be0a2b2ddfd5d4ce8a8c714bc3
alexander kovtun <kovtuna@getsorbet.com>
2023-01-12 02:18:24 +0200

    add google sign in

--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { LogoSvg } from "../../components/logoSvg";
 import signInImage from "../../images/signInPageImage.png";
 import "./styles.css";
+import jwt_decode from "jwt-decode";
 
 export const SingInPage = () => {
+  const callback = (res: any) => {
+    console.log("bla res", jwt_decode(res.credential));
+  };
+  useEffect(() => {
+    // @ts-ignore
+    google.accounts.id.initialize({
+      client_id:
+        "284915362588-n696c2mt9buef8c9bpgd9voia1j9ncq6.apps.googleusercontent.com",
+      callback: callback,
+    });
+    // @ts-ignore
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
+
   return (
     <div className="signIn-container">
       <div className="signIn-leftSide">
@@ -14,6 +32,8 @@ export const SingInPage = () => {
           <p className="signIn-subtitle">
             Advance your unused PTO and get<br></br>an extra $50 on us!
           </p>
+
+          <div id={"signInDiv"}></div>
         </div>
       </div>
       <div className="signIn-rightSide">
